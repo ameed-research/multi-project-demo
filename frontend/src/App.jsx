6f1e7d0bd67c3180120a2115f2bd9a6ee3d73993
Ameed <ameed.software@gmail.com>
2026-04-07 09:29:07 +0300

    adding react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="app">
+      <h1>Ameed</h1>
+      <p>This app will be bundled into Spring Boot static resources during Maven build.</p>
+    </main>
+  );
+}
+
+export default App;
+
