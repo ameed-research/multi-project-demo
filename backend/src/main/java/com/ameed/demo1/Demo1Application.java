55eaceec93c95988b5fb7bf07f680c3e5ecd38b6
Ameed <ameed.software@gmail.com>
2026-04-07 13:17:32 +0300

    testing azure files #4

--- backend/src/main/java/com/ameed/demo1/Demo1Application.java
+++ backend/src/main/java/com/ameed/demo1/Demo1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
     public static void main(String[] args) {
+        System.out.println("DEBUG: STORAGE_ACCOUNT_NAME = " + System.getenv("STORAGE_ACCOUNT_NAME"));
         SpringApplication.run(Demo1Application.class, args);
     }
 
