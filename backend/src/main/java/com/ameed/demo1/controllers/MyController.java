dee81ff1a25165c588af78248a60bfe2495fb75a
Ameed <ameed.software@gmail.com>
2026-04-07 10:55:21 +0300

    adding github actions #4

--- backend/src/main/java/com/ameed/demo1/controllers/MyController.java
+++ backend/src/main/java/com/ameed/demo1/controllers/MyController.java
@@ -1,11 +1,13 @@
 package com.ameed.demo1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/test")
 public class MyController {
 
     @GetMapping
